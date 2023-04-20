0282d74ff2bafa104ba73dabc4896e7675600ffc
Nathan <nathanyong04@gmail.com>
2023-04-20 11:35:44 -0400

    JQuery set up

--- openChat.js
+++ openChat.js
@@ -1,9 +1,26 @@
-window.document.addEventListener("DOMContentLoaded", domLoaded);
 
-function domLoaded() {
+$(function() {
+    $("#sendText").keyup(pushText);
+
+    $("#listenButton").click(pullText);
+
+});
+
+
+
+function pushText() {
+    let username = $("#username");
+    let password = $("#password");
+    let text = $("#sendText").val();
+    
 
 }
 
+
+
+function pullText() {
+
+}
 
 
 
